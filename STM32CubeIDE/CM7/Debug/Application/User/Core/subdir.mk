f420140461c868ac1cd6d872dcb80ad60dcc1f1e
Joon65 <joondong.lee@outlook.com>
2026-07-01 16:22:52 +0900

    v1 - Add printf () - UART_RX : PD8   UART_TX : PD9

---
+++ STM32CubeIDE/CM7/Debug/Application/User/Core/subdir.mk
@@ -0,0 +1,55 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (14.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Src/gpio.c \
+C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Src/main.c \
+C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Src/stm32h7xx_hal_msp.c \
+C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Src/stm32h7xx_it.c \
+../Application/User/Core/syscalls.c \
+../Application/User/Core/sysmem.c \
+C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Src/usart.c 
+
+OBJS += \
+./Application/User/Core/gpio.o \
+./Application/User/Core/main.o \
+./Application/User/Core/stm32h7xx_hal_msp.o \
+./Application/User/Core/stm32h7xx_it.o \
+./Application/User/Core/syscalls.o \
+./Application/User/Core/sysmem.o \
+./Application/User/Core/usart.o 
+
+C_DEPS += \
+./Application/User/Core/gpio.d \
+./Application/User/Core/main.d \
+./Application/User/Core/stm32h7xx_hal_msp.d \
+./Application/User/Core/stm32h7xx_it.d \
+./Application/User/Core/syscalls.d \
+./Application/User/Core/sysmem.d \
+./Application/User/Core/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Application/User/Core/gpio.o: C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Src/gpio.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H755xx -DUSE_PWR_DIRECT_SMPS_SUPPLY -c -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc/Legacy" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Device/ST/STM32H7xx/Include" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/main.o: C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Src/main.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H755xx -DUSE_PWR_DIRECT_SMPS_SUPPLY -c -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc/Legacy" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Device/ST/STM32H7xx/Include" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/stm32h7xx_hal_msp.o: C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Src/stm32h7xx_hal_msp.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H755xx -DUSE_PWR_DIRECT_SMPS_SUPPLY -c -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc/Legacy" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Device/ST/STM32H7xx/Include" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/stm32h7xx_it.o: C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Src/stm32h7xx_it.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H755xx -DUSE_PWR_DIRECT_SMPS_SUPPLY -c -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc/Legacy" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Device/ST/STM32H7xx/Include" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/%.o Application/User/Core/%.su Application/User/Core/%.cyclo: ../Application/User/Core/%.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H755xx -DUSE_PWR_DIRECT_SMPS_SUPPLY -c -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc/Legacy" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Device/ST/STM32H7xx/Include" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/usart.o: C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Src/usart.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DCORE_CM7 -DUSE_HAL_DRIVER -DSTM32H755xx -DUSE_PWR_DIRECT_SMPS_SUPPLY -c -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/STM32H7xx_HAL_Driver/Inc/Legacy" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/CM7/Core/Inc" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Device/ST/STM32H7xx/Include" -I"C:/workspace/tenic/tenic_motorcontrol_FOC_20260701/stm32h755_foc_algorithm/Drivers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Application-2f-User-2f-Core
+
+clean-Application-2f-User-2f-Core:
+	-$(RM) ./Application/User/Core/gpio.cyclo ./Application/User/Core/gpio.d ./Application/User/Core/gpio.o ./Application/User/Core/gpio.su ./Application/User/Core/main.cyclo ./Application/User/Core/main.d ./Application/User/Core/main.o ./Application/User/Core/main.su ./Application/User/Core/stm32h7xx_hal_msp.cyclo ./Application/User/Core/stm32h7xx_hal_msp.d ./Application/User/Core/stm32h7xx_hal_msp.o ./Application/User/Core/stm32h7xx_hal_msp.su ./Application/User/Core/stm32h7xx_it.cyclo ./Application/User/Core/stm32h7xx_it.d ./Application/User/Core/stm32h7xx_it.o ./Application/User/Core/stm32h7xx_it.su ./Application/User/Core/syscalls.cyclo ./Application/User/Core/syscalls.d ./Application/User/Core/syscalls.o ./Application/User/Core/syscalls.su ./Application/User/Core/sysmem.cyclo ./Application/User/Core/sysmem.d ./Application/User/Core/sysmem.o ./Application/User/Core/sysmem.su ./Application/User/Core/usart.cyclo ./Application/User/Core/usart.d ./Application/User/Core/usart.o ./Application/User/Core/usart.su
+
+.PHONY: clean-Application-2f-User-2f-Core
+
